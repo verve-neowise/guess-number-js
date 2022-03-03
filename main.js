e618e8dfe801531916065591c60a52ec8a5a2ce7
Neowise <verve.neowise@gmail.com>
2022-03-04 00:28:11 +0500

    refactor code

--- main.js
+++ main.js
@@ -1,40 +1,76 @@
 const $ = (selector) => document.querySelector(selector)
+const random = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
 
 const balance = $('#balance')
 const result  = $('#result')
 const bet     = $('#bet')
 const number  = $('#number')
 
-const submit = $('.btn.btn-success')
+const submit  = $('.btn.btn-success')
 
-const toastBtn = $('#liveToastBtn')
+let money  =  100
 
-let money   = 100
-let message = "Son kiriting"
-let random  = 0
-let guess   = 0
-
-updateUI()
+updateUI('')
 
 submit.onclick = () => {
     let userBet     = +bet.value
     let userNumber  = +number.value 
 
-    if (userBet === 0 || userNumber === 0 || isNaN(userBet) || isNaN(userNumber)) {
-        message = 'Ma`lumotlar xa`to kiritildi'
+    let error = checkData(userBet, userNumber)
+
+    if (error == '') {
+
+        let guess = random(0, 6)
+
+        if (playGame(userBet, userNumber, guess)) {
+            updateUI('Yutdingiz!')
+        }
+        else {
+            updateUI('Yutqizdingiz, son ' + guess + ' edi :(')
+        }
     }
-    else if (userBet < 5) {
-        message = '$5 dan kam qabul qilinmaydi'
+    else {
+        updateUI(error)
     }
-    else if (userNumber < 0 || userNumber > 6) {
-        message = '0 dan 6 gacha son!'
-    }
-    updateUI()
+
+    clearEntries()
 }
 
-function checkData()
+function checkData(userBet, userNumber) {
 
-function updateUI() {
-    balance.innerText = `$${balance}`
+    if (userBet === 0 || userNumber === 0 || isNaN(userBet) || isNaN(userNumber)) {
+        return 'Ma`lumotlar xa`to kiritildi'
+    }
+    else if (userBet < 5) {
+        return '$5 dan kam qabul qilinmaydi'
+    }
+    else if (userNumber < 0 || userNumber > 6) {
+        return '0 dan 6 gacha son!'
+    }
+    else if (userBet > money) {
+        return 'Mablag`ingiz yetarli emas!'
+    }
+
+    return ''
+}
+
+function playGame(userBet, userNumber, guess) {
+
+    if (guess === userNumber) {
+        money += userBet * 1.25
+        return true
+    }
+
+    money -= userBet
+    return false
+}
+
+function clearEntries() {
+    bet.value = ''
+    number.value = ''
+}
+
+function updateUI(message) {
+    balance.value = `$${money}`
     result.innerText  = `${message}`
 }
